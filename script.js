db24b975eba7b6c4d4511b21169c9f12b4720941
Brian Short <brian.ralph.short@gmail.com>
2020-09-27 22:36:32 -0400

    Finished html buttons for flipping to the next page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var secondsLeft = 100;
 
 var totalScore = 0; 
 
-var questionCounter = 0;
+// var questionCounter = 0;
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -15,61 +15,130 @@ function setTime() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      questionCounter = 5; 
+      gameOver(); 
     }
 
   }, 1000);
 }
 
-function quizBegin() {
-	document.getElementById("quizContent").remove();
-	var pageOne = document.createElement("div");
-	document.getElementById("quizZone").appendChild(pageOne);
+
+
+
+
+// function quizBegin() {
+// 	document.getElementById("quizContent").remove();
+// 	var pageOne = document.createElement("div");
+// 	document.getElementById("quizZone").appendChild(pageOne);
 	
-	var question = document.createElement("h3");
-	pageOne.appendChild(question);
-	pageOne.childNodes[0].textContent = "Question 1: What is HTML?";
+// 	var question = document.createElement("h3");
+// 	pageOne.appendChild(question);
+// 	pageOne.childNodes[0].textContent = "Question 1: What is HTML?";
 	
-	var answerOne = document.createElement("button");
-	pageOne.appendChild(answerOne);
-	pageOne.childNodes[1].textContent = "Hypertext Markup Language";
-	pageOne.childNodes[1].setAttribute("id" , "answerOne");
-	document.getElementById("answerOne").addEventListener("click" , function() {
-		console.log("Correct!");
-	})
+// 	var answerOne = document.createElement("button");
+// 	pageOne.appendChild(answerOne);
+// 	pageOne.childNodes[1].textContent = "Hypertext Markup Language";
+// 	pageOne.childNodes[1].setAttribute("id" , "answerOne");
+// 	document.getElementById("answerOne").addEventListener("click" , function() {
+// 		console.log("Correct!");
+// 		questionCounter++;
+// 		totalScore+=1000;
+// 	})
 
-	var answerTwo = document.createElement("button");
-	pageOne.appendChild(answerTwo);
-	pageOne.childNodes[2].textContent = "The basic structure of a webpage";
-	pageOne.childNodes[2].setAttribute("id" , "answerTwo");
-	document.getElementById("answerTwo").addEventListener("click" , function() {
-		console.log("Correct!");
-	})
+// 	var answerTwo = document.createElement("button");
+// 	pageOne.appendChild(answerTwo);
+// 	pageOne.childNodes[2].textContent = "The basic structure of a webpage";
+// 	pageOne.childNodes[2].setAttribute("id" , "answerTwo");
+// 	document.getElementById("answerTwo").addEventListener("click" , function() {
+// 		console.log("Correct!");
+// 		questionCounter++;
+// 		totalScore+=1000;
+// 	})
 
-	var answerThree = document.createElement("button");
-	pageOne.appendChild(answerThree);
-	pageOne.childNodes[3].textContent = "The first chapter in many web developer bootcamps";
-	pageOne.childNodes[3].setAttribute("id" , "answerThree");
-	document.getElementById("answerThree").addEventListener("click" , function() {
-		console.log("Correct!");
-	})
+// 	var answerThree = document.createElement("button");
+// 	pageOne.appendChild(answerThree);
+// 	pageOne.childNodes[3].textContent = "The first chapter in many web developer bootcamps";
+// 	pageOne.childNodes[3].setAttribute("id" , "answerThree");
+// 	document.getElementById("answerThree").addEventListener("click" , function() {
+// 		console.log("Correct!");
+// 		questionCounter++;
+// 		totalScore+=1000;
+// 	})
 
-	var answerFour = document.createElement("button");
-	pageOne.appendChild(answerFour);
-	pageOne.childNodes[4].textContent = "A kind of frog";
-	pageOne.childNodes[4].setAttribute("id" , "answerFour");
-	document.getElementById("answerFour").addEventListener("click" , function() {
-		console.log("Wrong!");
-	})
-}
+// 	var answerFour = document.createElement("button");
+// 	pageOne.appendChild(answerFour);
+// 	pageOne.childNodes[4].textContent = "A kind of frog";
+// 	pageOne.childNodes[4].setAttribute("id" , "answerFour");
+// 	document.getElementById("answerFour").addEventListener("click" , function() {
+// 		console.log("Wrong!");
+// 		questionCounter++;
+// 	})
+// }
 
-document.getElementById("quizStart").addEventListener ("click", function() {
-		setTime();
-		questionCounter++;
-		if (questionCounter === 1) {
-			quizBegin();
-		}
-	})
+// function quizTwo() {
+// 	pageOne.childNodes[0].textContent = "Question 2: What is CSS?";
+// 	pageOne.childNodes[1].textContent = "Cascading Style Sheets";
+// 	pageOne.childNodes[2].textContent = "A way to sculpt the appearance of a web page";
+// 	pageOne.childNodes[3].textContent = "An inline, internal, or external link to page styling elements";
+// 	pageOne.childNodes[4].textContent = "The official flower of California";
+// }
+
+// document.getElementById("quizStart").addEventListener ("click", function() {
+// 		setTime();
+// 		if (questionCounter === 0) {
+// 			document.getElementById("quizContent").remove();
+// 			var pageOne = document.createElement("div");
+// 			document.getElementById("quizZone").appendChild(pageOne);
+			
+// 			var question = document.createElement("h3");
+// 			pageOne.appendChild(question);
+// 			pageOne.childNodes[0].textContent = "Question 1: What is HTML?";
+			
+// 			var answerOne = document.createElement("button");
+// 			pageOne.appendChild(answerOne);
+// 			pageOne.childNodes[1].textContent = "Hypertext Markup Language";
+// 			pageOne.childNodes[1].setAttribute("id" , "answerOne");
+// 			document.getElementById("answerOne").addEventListener("click" , function() {
+// 				console.log("Correct!");
+// 				questionCounter++;
+// 				totalScore+=1000;
+// 			})
+
+// 			var answerTwo = document.createElement("button");
+// 			pageOne.appendChild(answerTwo);
+// 			pageOne.childNodes[2].textContent = "The basic structure of a webpage";
+// 			pageOne.childNodes[2].setAttribute("id" , "answerTwo");
+// 			document.getElementById("answerTwo").addEventListener("click" , function() {
+// 				console.log("Correct!");
+// 				questionCounter++;
+// 				totalScore+=1000;
+// 			})
+
+// 			var answerThree = document.createElement("button");
+// 			pageOne.appendChild(answerThree);
+// 			pageOne.childNodes[3].textContent = "The first chapter in many web developer bootcamps";
+// 			pageOne.childNodes[3].setAttribute("id" , "answerThree");
+// 			document.getElementById("answerThree").addEventListener("click" , function() {
+// 				console.log("Correct!");
+// 				questionCounter++;
+// 				totalScore+=1000;
+// 			})
+
+// 			var answerFour = document.createElement("button");
+// 			pageOne.appendChild(answerFour);
+// 			pageOne.childNodes[4].textContent = "A kind of frog";
+// 			pageOne.childNodes[4].setAttribute("id" , "answerFour");
+// 			document.getElementById("answerFour").addEventListener("click" , function() {
+// 				console.log("Wrong!");
+// 				questionCounter++;
+// 			})
+// 		} else if (questionCounter === 1){
+// 			pageOne.childNodes[0].textContent = "Question 2: What is CSS?";
+// 			pageOne.childNodes[1].textContent = "Cascading Style Sheets";
+// 			pageOne.childNodes[2].textContent = "A way to sculpt the appearance of a web page";
+// 			pageOne.childNodes[3].textContent = "An inline, internal, or external link to page styling elements";
+// 			pageOne.childNodes[4].textContent = "The official flower of California";
+// 		}
+// 	})
 
 
 	//add event listener for button () {
